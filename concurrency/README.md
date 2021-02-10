34bb1276454f5b06c8846011920a11eb1556c3bf
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-02-10 20:10:54 -0300

    Working with Mutex

--- concurrency/README.md
+++ concurrency/README.md
@@ -2,6 +2,18 @@
 
 This is a project to learn about some Golang concurrency concepts.
 
+## Go routines
+
+To create a go routine just adds the **go** keyword before the function.
+
+Example:
+
+```go
+... 
+    go something()
+...
+```
+
 ## Race conditions
 
 To check if there are race conditions in the program, run the code with command below.
--- concurrency/main.go
+++ concurrency/main.go
@@ -10,7 +10,15 @@ import (
 
 var wg sync.WaitGroup
 
+var result int
+var m sync.Mutex
+
 func main() {
+	//WaitGroup()
+	Mutex()
+}
+
+func WaitGroup()  {
 	fmt.Println(runtime.NumCPU())
 	wg.Add(2)
 	go RunProcess("P1", 20)
@@ -18,11 +26,23 @@ func main() {
 	wg.Wait()
 }
 
+func Mutex()  {
+	go RunProcess("P1", 20)
+	go RunProcess("P2", 20)
+
+	var s string
+	fmt.Scanln(&s)
+	fmt.Println("Final result:", result)
+}
+
 func RunProcess(name string, total int)  {
 	for i := 0; i < total; i++ {
-		fmt.Println(name, "->", i)
 		t := time.Duration(rand.Intn(255))
 		time.Sleep(time.Millisecond * t)
+		m.Lock()
+		result++
+		fmt.Println(name, "->", i, "Partial result:", result)
+		m.Unlock()
 	}
-	wg.Done()
+	//wg.Done()
 }
